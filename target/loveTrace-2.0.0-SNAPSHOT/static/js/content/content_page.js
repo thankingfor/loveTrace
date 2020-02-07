5a94d927c1fa2612e68d5048bc6927aae44dcbdb
liyanfu <984213466@qq.com>
2020-02-07 18:33:04 +0800

    modify

--- target/loveTrace-2.0.0-SNAPSHOT/static/js/content/content_page.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-	var imgs = getImages(images);
-	for(i = 0 ;i <  imgs.length; i ++){
-		$('#images').append("<img style='width:400;height:300px;' src="+imgs[i]+" />");
-	}
-})
--- target/loveTrace-2.0.0-SNAPSHOT/static/js/story/story_page.js
+++
@@ -1,7 +0,0 @@
-/*window.onresize = function(){
-    var imgs = document.getElementsByTagName("img");
-    var contentLeft = document.getElementById("myarticle");
-    for(var i=0; i<imgs.length; i++){
-        imgs[i].style["max-width"] = contentLeft.offsetWidth - 40 + "px";
-    }   
-}*/
